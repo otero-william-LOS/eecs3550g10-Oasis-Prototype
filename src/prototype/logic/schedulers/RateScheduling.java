62b9454b6443474af626fe82e27e0a19db2aa2cb
agray26 <agray26@NE103910.utad.utoledo.edu>
2019-05-01 19:49:13 -0400

    Added all methods for rate scheduler

--- src/prototype/logic/schedulers/RateScheduling.java
+++ src/prototype/logic/schedulers/RateScheduling.java
@@ -4,6 +4,12 @@
  * and open the template in the editor.
  */
 package prototype.logic.schedulers;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.ArrayList;
+import prototype.data.persistence.EntityDatabase;
+import prototype.data.drivers.RateDriver;
+import prototype.data.models.RateModel;
 
 /**
  *
@@ -29,6 +35,60 @@ public class RateScheduling implements Scheduler {
     @Override
     public void runModuleUserStories() {
         //  TODO will be updated as new user stories are created.
+    }
+    
+    public String setRate(LocalDate day, double rate){
+        String returnString = ""; // return this shit to the output boxi  think will be there i nthe gui
+        RateModel currentRate = RateDriver.searchByDate(day);
+        
+        if(day.isBefore(LocalDate.now())){
+            returnString = "Cant modify rates before today";
+        }
+        
+        // check to see if the rate for this day was already set
+        if(currentRate.getRateDate() != LocalDate.MIN){
+            // rate already exists so modify its values
+            RateDriver.modifyBaseRate(currentRate, rate);
+            returnString = "The base rate for " + day.toString() + " was modified to " + rate;
+        }
+        else{
+            // rate does not exist so add a new one
+            RateDriver.createRate(day, rate);
+            returnString = "The base rate for " + day.toString() + " was set to " + rate;
+        }
+                           
+        return returnString;
+    }
+    
+    public String setRateRange(LocalDate startDate, LocalDate endDate, double rate){
+    
+        String returnString = ""; // return this shit to the output boxi  think will be there i nthe gui
+        LocalDate currentDay = startDate;
+        RateModel currentRate = new RateModel();
+        
+        if(startDate.isBefore(LocalDate.now())){
+            returnString = "Cant modify rates before today";
+        }
+        
+        // iterate trhough the date range, modifying or creating dates as needed
+        while(currentDay != endDate.plusDays(1)){
+            currentRate = RateDriver.searchByDate(currentDay);
+            // check to see if the rate for this day was already set
+            if(currentRate.getRateDate() == LocalDate.MIN){
+                // rate already exists so modify its values
+                RateDriver.modifyBaseRate(currentRate, rate);
+            }
+            else{
+                // rate does not exist so add a new one
+                RateDriver.createRate(currentDay, rate);
+            }
+            
+            // advance to the next day
+            currentDay = currentDay.plusDays(1);
+        }
+
+        returnString = "The rates for " + startDate.toString() + " through " + endDate.toString() + " have been set to " + rate;
+        return returnString;
     }
     
 }
